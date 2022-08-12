e8eebb8744a3c83c427c79e7140ad83fbd1855c9
stepan7 <fenixrnd@mail.ru>
2022-08-12 08:33:37 +0300

    add write file block

--- blocks/Creator.cpp
+++ blocks/Creator.cpp
@@ -4,6 +4,7 @@
 #include "creators/ReadFileCreator.h"
 #include "creators/ReplaceBlockCreator.h"
 #include "creators/GrepBlockCreator.h"
+#include "creators/WriteFileBlockCreator.h"
 
 
 std::unique_ptr<Creator> Creator::mapper(const std::string& command_name) {
@@ -11,6 +12,7 @@ std::unique_ptr<Creator> Creator::mapper(const std::string& command_name) {
     creators["readfile"] = std::make_unique<ReadFileCreator>();
     creators["replace"] = std::make_unique<ReplaceBlockCreator>();
     creators["grep"] = std::make_unique<GrepBlockCreator>();
+    creators["writefile"] = std::make_unique<WriteFileBlockCreator>();
 
     return std::move(creators[command_name]);
 }
